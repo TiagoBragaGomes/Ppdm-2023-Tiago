c62b2395533a5efe99923fb031e067821dc11fa2
TiagoBragaGomes <tiago.g@gsuite.iff.edu.br>
2023-11-10 14:53:12 -0300

    atualização

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function telainicial() {
   return (
     <View style={styles.container}>
       <Text>App funcionando</Text>
-      <Text>Teste</Text>
+      <button title = "ir" Onpress>  Cadastro</button>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+function teladecadastro() {
+  return (
+    <View style={styles.container}>
+      <Text> App funcionando</Text>
+      <button>Teste</button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function App() {
+
+const Stack = createNativeStackNavigator();
+   return(
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Inicio" component={telainicial} />
+      <Stack.Screen name="Cadastro" component={teladecadastro} />
+    </Stack.Navigator>
+    </NavigationContainer>
+   
+   );
+  }
 
 const styles = StyleSheet.create({
   container: {
