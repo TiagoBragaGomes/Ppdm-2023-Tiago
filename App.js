2fe8eeb72d651b483fcf86dc26ad3e63be546a70
TiagoBragaGomes <tiago.g@gsuite.iff.edu.br>
2023-11-10 14:04:38 -0300

    Atualização

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>VOLTA PRA MIM</Text>
+      <Text>App funcionando</Text>
       <Text>Teste</Text>
       <StatusBar style="auto" />
     </View>
